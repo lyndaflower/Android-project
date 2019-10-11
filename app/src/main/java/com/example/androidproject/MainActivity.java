1369ede0911b16609e715d02ad125c256b24fcc7
lyndaflower <umurerwalynda@gmail.com>
2019-10-11 19:55:18 +0200

    add another activity

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView lyndaView;
+    private Button roxanneView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lyndaView= (TextView) findViewById(R.id.view);
+        roxanneView = (Button) findViewById(R.id.findButton);
+        roxanneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
