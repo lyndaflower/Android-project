b7e2b227d9ef5acb75d4033752d065f704c8add2
Agnes Nyiramisigaro <umurerwalynda@gmail.com>
2019-10-12 21:37:24 +0200

    add edit text and button

--- app/src/main/java/com/example/androidproject/AppActivity.java
+++ app/src/main/java/com/example/androidproject/AppActivity.java
@@ -3,12 +3,20 @@ package com.example.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AppActivity extends AppCompatActivity {
+
+ @BindView(R.id.makeChoice) Button mMakeChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        ButterKnife.bind(this);
     }
 }
