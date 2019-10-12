a34875b646f658e06e6c298c484490d77c144319
lyndaflower <umurerwalynda@gmail.com>
2019-10-12 11:21:11 +0200

    add images

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView lyndaView;
-    private Button roxanneView;
+
+    @BindView(R.id.view) TextView lyndaView;
+    @BindView(R.id.findButton) Button roxanneView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lyndaView= (TextView) findViewById(R.id.view);
-        roxanneView = (Button) findViewById(R.id.findButton);
+        ButterKnife.bind(this);
         roxanneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent gallery = new Intent(MainActivity.this, GalleryActivity.class );
+                startActivity(gallery);
 
             }
         });
