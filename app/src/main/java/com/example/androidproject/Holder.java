ba7d096e78dbd1661ea1d86e8a8d048e01d2fef4
lyndaflower <umurerwalynda@gmail.com>
2019-10-18 17:07:50 +0200

    add a change

--- app/src/main/java/com/example/androidproject/Holder.java
+++
@@ -1,6 +0,0 @@
-package com.example.androidproject;
-
-public class Holder {
-    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
-    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
-}
