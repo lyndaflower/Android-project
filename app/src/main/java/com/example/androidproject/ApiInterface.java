270ae5c4452ef9b729bcfaa6e5f5041789c9d04c
lyndaflower <umurerwalynda@gmail.com>
2019-10-18 14:24:00 +0200

    add class to make network request

--- app/src/main/java/com/example/androidproject/ApiInterface.java
+++ app/src/main/java/com/example/androidproject/ApiInterface.java
@@ -1,4 +1,15 @@
 package com.example.androidproject;
 
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiInterface {
+    @GET("businesses/search")
+    Call<YelpPurabella> getPerfumes(
+            @Query("location") String location,
+            @Query("term") String term
+
+    );
 }
